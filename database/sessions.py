444cccbec7ac356e5d920875279206e2c703ecff
BrenDev0 <lahey1991@gmail.com>
2025-06-19 17:04:59 -0600

    env variable

--- database/sessions.py
+++ database/sessions.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, Session
 import os
 from typing import Generator
 
-DB_URL = os.getenv("DB_URL")
+DB_URL = os.getenv("DATABASE_URL")
 print(DB_URL)
 engine = create_engine(DB_URL, pool_pre_ping=True)  
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
