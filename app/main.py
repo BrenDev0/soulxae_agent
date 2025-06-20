5d5e6fa5426ed8554a788eba69a73ae154c4ec72
BrenDev0 <lahey1991@gmail.com>
2025-06-20 10:34:27 -0600

    session

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from routes import interact
 from dependencies.container import Container
+from database.sessions import get_db_session
 
 
 app = FastAPI()
@@ -13,8 +14,9 @@ origins = [
     "http://localhost:3000",
     "soulxae.up.railway.app"
 ]
+session = get_db_session()
 
-Container = Container()
+Container = Container(db_session=session)
 Container.tools_service.configure_tools()
 
 app.add_middleware(
