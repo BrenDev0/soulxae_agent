1b0d25fa180151461726ac225cc6c95b1febdf18
BrenDev0 <lahey1991@gmail.com>
2025-06-20 14:42:18 -0600

    printing embedding

--- services/embedding_service.py
+++ services/embedding_service.py
@@ -23,6 +23,7 @@ class EmbeddingService:
 
     async def add_tool(self, tool_id: str, description: str, metadata: Dict = {}):
         embedding = await self.get_embedding(description)
+        print(f"{embedding}::::::::::::: embedding")
         self.tool_index.add(np.array([embedding], dtype=np.float32))
 
         index = len(self.tool_metadata)
