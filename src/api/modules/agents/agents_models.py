e7a0c2235abed15a323a7108a7888e14398c045a
BrenDev0 <lahey1991@gmail.com>
2025-08-07 17:23:14 -0600

    removed unused agent models

--- src/api/modules/agents/agents_models.py
+++ src/api/modules/agents/agents_models.py
@@ -1,29 +1,4 @@
 from pydantic import BaseModel
-from typing import List, Any
-from sqlalchemy import Column, String, Text, ForeignKey, Integer, Float
-from sqlalchemy.ext.declarative import declarative_base
-from src.agent.state import State
-
-
 
 class InteractionRequest(BaseModel):
     conversation_id: str
-
-class LLMConfig(BaseModel):
-    prompt: str;
-    tools: List[Any];
-    max_tokens: int;
-    temperature: float
-
-
-# db 
-
-Base = declarative_base()
-
-class AgentConfig(Base):
-    __tablename__ = 'ai_config'
-    ai_config_id = Column(String, primary_key=True)
-    agent_id = Column(String, ForeignKey('agents.agent_id'))
-    system_prompt = Column(Text)
-    max_tokens = Column(Integer)
-    temperature = Column(Float)
